36c5d6a736138e0823d1876f92fc185cd299f05b
Wee Seng <75309293+Akimori-236@users.noreply.github.com>
2022-03-18 14:37:13 +0800

    Delete Photo Website Portfolio/config directory

--- Photo Website Portfolio/config/settings.py
+++
@@ -1,7 +0,0 @@
-#centralised settings as diff service providers will have diff database and keys
-
-class Settings:
-    secretKey = 'What1The2Duck3Bar4be5que69'
-    database='sp_movies'
-    user='root'
-    password='230689'
